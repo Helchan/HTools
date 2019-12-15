d62b14d0cc13f8f6053a4ec69486f5f05c1121ff
Helchan <875633531@qq.com>
2019-12-15 08:57:16 +0800

    配置窗体

---
+++ src/main/java/com/heltec/tools/main/MainEntr.java
@@ -0,0 +1,47 @@
+package com.heltec.tools.main;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainEntr extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainEntr frame = new MainEntr();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainEntr() {
+		//窗体标题
+		setTitle("HTools");
+		//应用随窗体关闭而关闭
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		//窗体居中
+		this.setLocationRelativeTo(null);
+		setContentPane(contentPane);
+	}
+
+}
